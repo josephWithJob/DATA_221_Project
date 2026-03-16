8502bbb5aa70f83a87d13705be980053bacd72a7
josephWithJob <joseph.krosel@gmail.com>
2026-03-16 16:06:07 -0600

    Added code to read the csv file in and seperates the data into the feature matrix and target matrix for the ML models

---
+++ Main.py
@@ -0,0 +1,19 @@
+# Data website link: https://www.kaggle.com/datasets/teejmahal20/airline-passenger-satisfaction?resource=download
+
+# Library Imports
+import pandas
+
+# Reads the csv
+airline_data_csv = pandas.read_csv("AirlineData.csv")
+
+# Separates the data
+satisfaction_airline_data_csv = airline_data_csv['satisfaction']
+# classification_data_from_kidney_diseases_file = data_from_kidney_diseases_file['classification'].to_numpy()
+feature_data_from_airline_data_csv = airline_data_csv.drop('satisfaction', axis=1)
+
+# Turns classification data into 0's and 1's
+satisfaction_airline_data_csv = satisfaction_airline_data_csv.replace({"satisfied":1, "neutral or dissatisfied":0})
+
+feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.replace({"Male":0, "Female":1, "disloyal Customer":0, "Loyal Customer":1,
+                                                               "Personal Travel":0, "Business travel":1, "Eco":0, "Eco Plus":0.5,
+                                                               "Business":1})

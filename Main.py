f8dfed44d90a08a7055f9d7ac1a48dd4393f5a6f
josephWithJob <joseph.krosel@gmail.com>
2026-03-16 17:21:38 -0600

    Added code that removes the first two columns of the feature matrix because they are useless to the learning of the model

--- Main.py
+++ Main.py
@@ -2,18 +2,34 @@
 
 # Library Imports
 import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # Reads the csv
 airline_data_csv = pandas.read_csv("AirlineData.csv")
 
-# Separates the data
+# Separates the data for the target data
 satisfaction_airline_data_csv = airline_data_csv['satisfaction']
-# classification_data_from_kidney_diseases_file = data_from_kidney_diseases_file['classification'].to_numpy()
+
+# Separates the feature data and removes unnecessary columns of id number and first column unnamed.
 feature_data_from_airline_data_csv = airline_data_csv.drop('satisfaction', axis=1)
+feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.drop('id', axis=1)
+feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.iloc[:, 1:]
 
 # Turns classification data into 0's and 1's
 satisfaction_airline_data_csv = satisfaction_airline_data_csv.replace({"satisfied":1, "neutral or dissatisfied":0})
 
 feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.replace({"Male":0, "Female":1, "disloyal Customer":0, "Loyal Customer":1,
-                                                               "Personal Travel":0, "Business travel":1, "Eco":0, "Eco Plus":0.5,
-                                                               "Business":1})
+                                                                                 "Personal Travel":0, "Business travel":1, "Eco":0, "Eco Plus":0.5,
+                                                                                 "Business":1})
+
+# Splits data for training ML models
+features_train, features_test, labels_train, labels_test = train_test_split(feature_data_from_airline_data_csv,
+                                                                            satisfaction_airline_data_csv, test_size=0.33, random_state=42)
+
+
+
+
+

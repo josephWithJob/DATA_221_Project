cd3a376f3440804c6c384860ebe5dd8f38c72b22
Ccasend83 <lucasjoffre@hotmail.com>
2026-03-27 21:12:37 -0600

    Added the train_test_split function and made the feature and target data. Also replaced any categorical values with 0 or 1.

--- Logistic-Regression.py
+++ Logistic-Regression.py
@@ -24,7 +24,24 @@ satisfaction_airline_data_csv = satisfaction_airline_data_csv.replace({"satisfie
 feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.replace({"Male":0, "Female":1, "disloyal Customer":0, "Loyal Customer":1,
                                                                                  "Personal Travel":0, "Business travel":1, "Eco":0, "Eco Plus":0.5,
                                                                                  "Business":1})
+# force the satisfaction labels to be integers 0 and 1
+# any value that didn't get replaced by 0 or 1 will be caught here
+satisfaction_airline_data_csv = satisfaction_airline_data_csv.astype(int)
+
+# remove any row that has at least one missing value
+feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.dropna()
+
+
+# this removes the rows in our target columns that we've removed from the feature columns
+satisfaction_airline_data_csv = satisfaction_airline_data_csv[feature_data_from_airline_data_csv.index]
 
 # Splits data for training ML models
 features_train, features_test, labels_train, labels_test = train_test_split(feature_data_from_airline_data_csv,
                                                                             satisfaction_airline_data_csv, test_size=0.20, random_state=42)
+
+
+classifier = LogisticRegression(max_iter=1000)
+classifier.fit(features_train, labels_train)
+
+accuracy_score = classifier.score(features_test, labels_test)
+print(f"Model Accuracy: {accuracy_score}")

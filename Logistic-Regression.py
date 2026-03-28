4a7c3bfeab281e901244d2da530352eb29e28266
Ccasend83 <lucasjoffre@hotmail.com>
2026-03-27 20:58:30 -0600

    authorized and imported libraries for logistic regression

---
+++ Logistic-Regression.py
@@ -0,0 +1,9 @@
+# Author: Lucas Joffre
+# Date: March 27 2026
+# DATA221 Final Project - Logistic-Regression
+
+# Library Imports
+import pandas
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+

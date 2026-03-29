f8ff6fa3e88e3c9e47c5f5b6b7a21dcda0d90121
HarrisLaptop <harris.khan1@ucalgary.ca>
2026-03-29 12:50:51 -0600

    Edit code to print  evaluation metrics variables with the number of neighbours that produce the highest f1-score

--- KNN_Model.py
+++ KNN_Model.py
@@ -47,23 +47,45 @@ features_train, features_test, labels_train, labels_test = train_test_split(
 
 # ========================= KNN MODEL =========================
 
-# Define a KNN model that searches the 5 closest neighbours
-knn_model_of_customer_satisfaction = KNeighborsClassifier(n_neighbors=5)
+# Define a list of 8 different number of neighbours to use to define our KNN Model
+list_of_number_of_neighbors = [1,3,5,7,9,11,13, 15]
 
-# Train the KNN model using the training data
-trained_knn_model_of_customer_satisfaction = knn_model_of_customer_satisfaction.fit(features_train, labels_train)
+# Initialize evaluation metrics variables with the number of neighbours that produce the highest f1-score
+highest_f1_score_of_knn_model = 0.0
+accuracy_at_highest_f1_score = 0.0
+precision_at_highest_f1_score = 0.0
+recall_at_highest_f1_score = 0.0
 
-# Make predictions using the testing data
-predictions_of_knn_model = trained_knn_model_of_customer_satisfaction.predict(features_test)
+number_of_neighbors_at_highest_f1_score = 0
 
-# Calculate evaluation metrics
-accuracy_of_knn_model = accuracy_score(labels_test, predictions_of_knn_model)
-precision_of_knn_model = precision_score(labels_test, predictions_of_knn_model)
-recall_of_knn_model = recall_score(labels_test, predictions_of_knn_model)
-f1_score_of_knn_model = f1_score(labels_test, predictions_of_knn_model)
+for num_neighbors in list_of_number_of_neighbors:
+    # Define a KNN model that searches the selected number of closest neighbours
+    knn_model_of_customer_satisfaction = KNeighborsClassifier(n_neighbors=num_neighbors)
+
+    # Train the KNN model using the training data
+    trained_knn_model_of_customer_satisfaction = knn_model_of_customer_satisfaction.fit(features_train, labels_train)
+
+    # Make predictions using the testing data
+    predictions_of_knn_model = trained_knn_model_of_customer_satisfaction.predict(features_test)
+
+    # Calculate evaluation metrics
+    f1_score_of_knn_model = f1_score(labels_test, predictions_of_knn_model)
+    accuracy_of_knn_model = accuracy_score(labels_test, predictions_of_knn_model)
+    precision_of_knn_model = precision_score(labels_test, predictions_of_knn_model)
+    recall_of_knn_model = recall_score(labels_test, predictions_of_knn_model)
+
+    # Find the highest f1-score of all KNN Models and set their evaluation metric scores here
+    if f1_score_of_knn_model > highest_f1_score_of_knn_model:
+        highest_f1_score_of_knn_model = f1_score_of_knn_model
+        accuracy_at_highest_f1_score = accuracy_of_knn_model
+        precision_at_highest_f1_score = precision_of_knn_model
+        recall_at_highest_f1_score = recall_of_knn_model
+
+        number_of_neighbors_at_highest_f1_score = num_neighbors
 
 # Print the evaluation metrics
-print("Accuracy of the KNN model: ", accuracy_of_knn_model)
-print("Precision of the KNN model: ", precision_of_knn_model)
-print("Recall of the KNN model: ", recall_of_knn_model)
-print("F1 of the KNN model: ", f1_score_of_knn_model)
+print("Number of K-Nearest-Neighbors that Produce Highest F1-Score:", number_of_neighbors_at_highest_f1_score)
+print("Highest F1-Score of the KNN model: ", highest_f1_score_of_knn_model)
+print("Accuracy at the Highest F1-Score: ", accuracy_at_highest_f1_score)
+print("Precision at the Highest F1-Score: ", precision_at_highest_f1_score)
+print("Recall at the Highest F1-Score: ", recall_at_highest_f1_score)

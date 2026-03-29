0f5f1414084cc6c3bd960b369e0b6dd79ba89ca8
KyleWangUcalgary <kyle.wang@ucalgary.ca>
2026-03-28 22:47:15 -0600

    add comments and precision, recall and f1 score tests.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,7 +5,7 @@
 
 # Library Imports
 import pandas
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models
 
@@ -34,6 +34,7 @@ feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.drop('id
 feature_data_from_airline_data_csv = feature_data_from_airline_data_csv.iloc[:, 1:]
 
 
+
 # Splits data for training ML models
 features_train, features_test, labels_train, labels_test = train_test_split(
     feature_data_from_airline_data_csv,
@@ -41,16 +42,24 @@ features_train, features_test, labels_train, labels_test = train_test_split(
     test_size=0.2,
     random_state=42)
 
-model = models.Sequential([ #create neural network
+#creates neural network
+model = models.Sequential([
     layers.Input(shape = (22,)),
-    layers.Dense(12,activation = "relu"),
-    layers.Dense(6, activation = "relu"),
+    layers.Dense(11,activation = "relu"),
+    layers.Dense(5, activation = "relu"),
     layers.Dense(1, activation="sigmoid")
 ])
 
+#configure model for training.
 model.compile(optimizer = "adam", loss = "binary_crossentropy",metrics = ["accuracy"])
-model.fit(features_train,labels_train,validation_split =0.1,epochs = 10 ,batch_size = 32) #fit/train model
+
+#trains the model
+model.fit(features_train,labels_train,validation_split =0.1,epochs = 15 ,batch_size = 32) #fit/train model
+
+#predicts labels based on the model.
 test_predicted_labels = (model.predict(features_test) > 0.5).astype(int).flatten() #find the labels for all the predictions
 
-print(accuracy_score(test_predicted_labels,labels_test))
-
+print("Accuracy: ", accuracy_score(test_predicted_labels,labels_test))
+print("Precision: ", precision_score(test_predicted_labels,labels_test))
+print("Recall: ", recall_score(test_predicted_labels,labels_test))
+print("F1 Score: ", f1_score(test_predicted_labels, labels_test))
